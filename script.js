25ae59a2fb2c6b3fcbfc20d1e19e19aa3053dd4b
Shivkumarmedar <121854125+Shivkumarmedar@users.noreply.github.com>
2023-08-26 19:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-// This is the boilerplate code given for you
-// You can modify this code
-// Product data
-const products = [
-  { id: 1, name: "Product 1", price: 10 },
-  { id: 2, name: "Product 2", price: 20 },
-  { id: 3, name: "Product 3", price: 30 },
-  { id: 4, name: "Product 4", price: 40 },
-  { id: 5, name: "Product 5", price: 50 },
-];
+ function updateCartDisplay(cart) {
+            const cartList = document.getElementById('cart-list');
+            cartList.innerHTML = '';
 
-// DOM elements
-const productList = document.getElementById("product-list");
+            cart.forEach(item => {
+                const li = document.createElement('li');
+                li.textContent = `Product ${item.id} - $${item.price}`;
+                cartList.appendChild(li);
+            });
+        }
 
-// Render product list
-function renderProducts() {
-  products.forEach((product) => {
-    const li = document.createElement("li");
-    li.innerHTML = `${product.name} - $${product.price} <button class="add-to-cart-btn" data-id="${product.id}">Add to Cart</button>`;
-    productList.appendChild(li);
-  });
-}
+        // Function to handle adding items to the cart
+        function addToCart(event) {
+            const productId = event.target.parentElement.getAttribute('data-product-id');
+            const productPrice = parseInt(event.target.parentElement.textContent.match(/\d+/)[0]);
 
-// Render cart list
-function renderCart() {}
+            let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+            cart.push({ id: productId, price: productPrice });
 
-// Add item to cart
-function addToCart(productId) {}
+            sessionStorage.setItem('cart', JSON.stringify(cart));
+            updateCartDisplay(cart);
+        }
 
-// Remove item from cart
-function removeFromCart(productId) {}
+        // Add event listeners to "Add to Cart" buttons
+        const addToCartButtons = document.querySelectorAll('.add-to-cart');
+        addToCartButtons.forEach(button => {
+            button.addEventListener('click', addToCart);
+        });
 
-// Clear cart
-function clearCart() {}
-
-// Initial render
-renderProducts();
-renderCart();
+        // Initial update of cart display
+        const initialCart = JSON.parse(sessionStorage.getItem('cart')) || [];
+        updateCartDisplay(initialCart);
